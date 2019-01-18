c231ea5ba780a1de4c5820f1b1ce4e19d3ecece1
sovit-123 <sovitrath5@gmail.com>
2019-01-18 17:12:28 +0530

    CSS changes

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,12 +4,25 @@ import './Landing.css';
 
 const Landing = () => {
   return (
-    <div>
-      <img
-        src={require('../../assets/images/landing-books.png')}
-        alt="Stack of books"
-        className="landing-image"
-      />
+    <div className="landing-section">
+      <div className="image-section">
+        <img
+          src={require('../../assets/images/landing-books.png')}
+          alt="Stack of books"
+          className="landing-image"
+        />
+      </div>
+      <div className="description">
+        <p>
+          Want to spend a rainy Saturday morning or lazy Sunday afternoon with
+          book in one hand and coffee in the other ?<br />
+          Well, look no further, because you are at the right station. Just buy
+          your book and hop on to the Reading Train. <br /> 
+          <b>
+            <em>Your journey awaits you.</em>
+          </b>
+        </p>
+      </div>
     </div>
   );
 };
