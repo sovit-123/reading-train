ad705daf9e273e10424f299881c9dea2d75b061d
sovit-123 <sovitrath5@gmail.com>
2019-02-15 09:50:07 +0530

    Fetching Books working

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -8,25 +8,40 @@ let booksInfo = [];
 
 const Results = () => {
   const allBooks = store.getState().fetchState.books.items;
-  for (let i = 0; i < allBooks.length; i++) {
-    if (allBooks[i].volumeInfo.imageLinks !== undefined) {
-      booksInfo[i] = 0;
-      booksInfo[i] = allBooks[i].volumeInfo.imageLinks.thumbnail;
+  if (store.getState() && allBooks !== undefined) {
+    for (let i = 0; i < allBooks.length; i++) {
+      if (allBooks[i].volumeInfo.imageLinks !== undefined) {
+        booksInfo[i] = 0;
+        booksInfo[i] = allBooks[i].volumeInfo;
+      }
     }
   }
 
-    return (
-      <div className="images-box">
-        {booksInfo.map((images, key) => {
-          return (
-            <div>
-              <img src={images} alt="" key={key} className="thumb-images" />
-              <hr />
-            </div>
-          );
-        })}
-      </div>
-    );
+  return (
+    <div className="images-box">
+      {booksInfo.map((data, key = 0) => {
+        return (
+          <div>
+            <a href={data.infoLink} target="_blank" rel="noopener noreferrer">
+              <div className="book">
+                <img
+                  src={data.imageLinks.thumbnail}
+                  key={key + 1}
+                  alt=""
+                  className="thumb-images"
+                />
+                <span className="title-author">
+                  <p>{data.title}</p>
+                  <p className="author">{data.authors}</p>
+                </span>
+              </div>
+            </a>
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Results;
